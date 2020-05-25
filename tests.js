e96ee47a6e383ad3e9a92c5d47b005fd1547655e
Jonathan New <jonnew@gmail.com>
2020-05-24 22:57:36 -0700

    Order independant test. Includes brief test explanations and minor cleanup

--- tests.js
+++ tests.js
@@ -25,8 +25,6 @@ module.exports = {
 			const resp = await request(app).get('/all');
 			resolve(resp.body);
 		});
-
-		
 	},
 	testCommonMethods: function(adapter){
 		/**
@@ -41,14 +39,12 @@ module.exports = {
 
 		return valid
 	},
-
 	testUseMethod: function(adapter){
 		/**
 		 * Checks if router.use is an available function
 		 */
 		return typeof adapter.router.use === "function";
 	},
-
 	canBeUsedByExpress: function(adapter){
 		/**
 		 * Checks if the router itself can be used as an express middleware
@@ -63,7 +59,6 @@ module.exports = {
 		}
 		return true;
 	},
-
 	standardFunctionSignature: async function(adapter){
 		/**
 		 * Checks if req, res, next are the arguments of a router callback.
@@ -107,6 +102,9 @@ module.exports = {
 	},
 
 	paramRoutes: async function(adapter){
+		/**
+		 * Checks if a router can handle parameterized routes.
+		 */
 		return new Promise(async (resolve, reject) => {
 			const app = express();
 			let value;
@@ -118,12 +116,13 @@ module.exports = {
 
 			const resp = await request(app).get('/hello');
 			resolve(resp.text === "hello");
-		});
-
-		
+		});		
 	},
 
-	sameRouteTwice: function(adapter ){
+	sameRouteTwice: function(adapter){
+		/**
+		 * Checks if a router allows duplicate routes.
+		 */
 		const app = express();
 		adapter.buildGetRoute("/", ()=>{});
 
@@ -138,6 +137,9 @@ module.exports = {
 	},
 
 	multipleMiddlewares: function(adapter){
+		/**
+		 * Checks if a router allows for multiple middlewares.
+		 */
 		return new Promise(async (resolve, reject) => {
 			const app = express();
 			let firstMiddlewareUsed = false;
@@ -167,6 +169,9 @@ module.exports = {
 		
 	},
 	regexRoutes: async function(adapter){
+		/**
+		 * Checks if a router allows for regular expression routes
+		 */
 		return new Promise(async (resolve, reject) => {
 			const app = express();
 			try{
@@ -184,7 +189,33 @@ module.exports = {
 		});
 	},
 
+	orderIndependent: async function(adapter){
+		/**
+		 * Checks if a router allows for regular expression routes
+		 */
+		return new Promise(async (resolve, reject) => {
+			const app = express();
+			try{
+				adapter.buildGetRoute("/:someParam", function(...args){
+					adapter.send(false, args);
+				});
+				adapter.buildGetRoute("/test", function(...args){
+					adapter.send(true, args);
+				});
+				app.use(adapter.middleware());
+			}
+			catch(err){
+				resolve(false);
+			}
+
+			const resp = await request(app).get('/test');
+			resolve(resp.body);
+		});	
+	},
 	pathToRegexRoutes: async function(adapter){
+		/**
+		 * Checks if a router handles "path-to-regex" styles routes
+		 */
 		return new Promise(async (resolve, reject) => {
 			const app = express();
 			try{
@@ -203,10 +234,11 @@ module.exports = {
 			const resp = await request(app).get('/abbbcd');
 			resolve(resp.body);
 		});
-		
 	},
-
 	starRoute: async function(adapter){
+		/**
+		 * Checks if a router handles wildcard routes
+		 */
 		return new Promise(async (resolve, reject) => {
 			const app = express();
 			try{
@@ -222,8 +254,5 @@ module.exports = {
 			const resp = await request(app).get('/anything');
 			resolve(resp.body);
 		});
-
-		
 	}
-
 };
